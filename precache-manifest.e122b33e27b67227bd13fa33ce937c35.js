d3b1f917428641dc10cc2845373627b10f74bde9
Soheil <liam@ramsalt.com>
2021-12-20 02:52:45 +0330

    Updates

---
+++ precache-manifest.e122b33e27b67227bd13fa33ce937c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9bfb9e182b661c937bbcb5a4e8584b",
+    "url": "/reddit/index.html"
+  },
+  {
+    "revision": "426889294dff46f38da2",
+    "url": "/reddit/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "05327c3c7d96fe6100c9",
+    "url": "/reddit/static/css/main.24199816.chunk.css"
+  },
+  {
+    "revision": "426889294dff46f38da2",
+    "url": "/reddit/static/js/2.63d5e248.chunk.js"
+  },
+  {
+    "revision": "05327c3c7d96fe6100c9",
+    "url": "/reddit/static/js/main.232444e7.chunk.js"
+  },
+  {
+    "revision": "6220bc4009882e7446fe",
+    "url": "/reddit/static/js/runtime-main.c3e7a993.js"
+  }
+]);
